1e635a1b9190d3944a144cdfc9a48160322bd4c0
Denis Kochetov <dacab@mail.ru>
2016-06-16 17:28:24 +0400

    MVA Game 1.0

---
+++ MVA Game/Assets/heroScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class heroScript : MonoBehaviour {
+    public float speed = 10f;
+    Rigidbody2D rig;
+    // Use this for initialization
+    void Start () {
+        rig = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float move = Input.GetAxis("Horizontal");
+        rig.velocity = new Vector2(move * speed, rig.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rig.AddForce(new Vector2(0, 700f));
+        }
+	}
+}
