75a6d7ca3e39726402166726d496762cdca1b6f3
Denis Kochetov <dacab@mail.ru>
2016-06-17 18:48:06 +0400

    MVA Game 1.1

--- MVA Game/Assets/heroScript.cs
+++ MVA Game/Assets/heroScript.cs
@@ -3,7 +3,16 @@ using System.Collections;
 
 public class heroScript : MonoBehaviour {
     public float speed = 10f;
+
+    bool facingRight = true;
+
+    bool grounded = false;
+    public Transform groundCheck;
+    public float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+
     Rigidbody2D rig;
+
     // Use this for initialization
     void Start () {
         rig = GetComponent<Rigidbody2D>();
@@ -11,12 +20,27 @@ public class heroScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        float move = Input.GetAxis("Horizontal");
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+
+        float move;
+        move = Input.GetAxis("Horizontal");
         rig.velocity = new Vector2(move * speed, rig.velocity.y);
 
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow) && grounded)
         {
             rig.AddForce(new Vector2(0, 700f));
         }
+        if ((move < 0) && facingRight)
+            Flip();
+        else
+            if ((move > 0) && !facingRight)
+            Flip();
 	}
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
 }
